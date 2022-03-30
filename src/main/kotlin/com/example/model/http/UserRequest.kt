6cfa890d93930194deedc457e9b5fcd7de604093
byeonguhun <dksekfldks65@naver.com>
2022-03-30 15:49:56 +0900

    post api 구현

--- src/main/kotlin/com/example/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/model/http/UserRequest.kt
@@ -1,7 +1,15 @@
 package com.example.model.http
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)  // 스네이크 케이스로 매핑
 data class UserRequest(var name:String?=null,
                        var age:Int?=null,
                        var email:String?=null,
-                       var address:String?=null
+                       var address:String?=null,
+
+                       //@JsonProperty("phone_number")
+                       var phoneNumber:String?=null
 )
